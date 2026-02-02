dc688377410fc3ddf6e9032f2fff1df4ff2883d6
Fabio Cordoba <fabiocordoba1@gmail.com>
2026-02-02 16:05:26 -0500

    feat: Actualizar resultados de OCR y mejorar procesamiento de imágenes

--- test_paddleocr.py
+++ test_paddleocr.py
@@ -1,10 +1,26 @@
 
 from paddleocr import PaddleOCR
+import re
 
+def limpiar_texto(texto):
+    print("Texto original:", texto)
+    return re.sub(r"\s+", " ", texto.strip())
+
+
+# ocr = PaddleOCR(
+#     use_doc_orientation_classify=False, 
+#     use_doc_unwarping=False, 
+#     use_textline_orientation=False)
+
+# #para Dockerfile optimizado
 ocr = PaddleOCR(
-    use_doc_orientation_classify=False, 
-    use_doc_unwarping=False, 
-    use_textline_orientation=False) # 文本检测+文本识别
+    text_detection_model_name="PP-OCRv5_mobile_det",
+    text_recognition_model_name="PP-OCRv5_mobile_rec",
+    use_doc_orientation_classify=True,
+    use_doc_unwarping=True,
+    use_textline_orientation=True
+)
+
 # ocr = PaddleOCR(use_doc_orientation_classify=True, use_doc_unwarping=True) # 文本图像预处理+文本检测+方向分类+文本识别
 # ocr = PaddleOCR(use_doc_orientation_classify=False, use_doc_unwarping=False) # 文本检测+文本行方向分类+文本识别
 # ocr = PaddleOCR(
@@ -14,8 +30,17 @@ ocr = PaddleOCR(
 #     use_doc_unwarping=False,
 #     use_textline_orientation=False) # 更换 PP-OCRv5_mobile 模型
 # result = ocr.predict("./general_ocr_002.png")
-result = ocr.predict("./foto.jpg")
+result = ocr.predict("./placa.jpg")
 for res in result:
-    res.print()
-    res.save_to_img("output")
-    res.save_to_json("output")
+        print("==== RESULTADO OCR ====")
+        # Salida original
+        res.print()
+        # res.save_to_img("output")
+        res.save_to_json("output")
+
+        print("----- Diferentes formatos de salida -----")
+        print(res['rec_texts'][0])
+
+        # Texto simple
+        print("Texto detectado:", limpiar_texto(str(res['rec_texts'][0])))
+        print("Confianza:", round(float(res['rec_scores'][0]), 3))
